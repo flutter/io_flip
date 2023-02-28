535025e922600d3349271cb818fc5cc60df4f7e3
Will Hlas <75334479+willhlas@users.noreply.github.com>
2023-02-28 11:40:34 -0600

    feat(api): configure cors

--- api/routes/_middleware.dart
+++ api/routes/_middleware.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cards_repository/cards_repository.dart';
 import 'package:dart_frog/dart_frog.dart';
 import 'package:shelf_cors_headers/shelf_cors_headers.dart';
@@ -8,5 +10,21 @@ Handler middleware(Handler handler) {
   return handler
       .use(requestLogger())
       .use(provider<CardsRepository>((_) => cardsRepository))
-      .use(fromShelfMiddleware(corsHeaders()));
+      .use(
+        fromShelfMiddleware(
+          corsHeaders(
+            headers: {
+              ACCESS_CONTROL_ALLOW_ORIGIN: _corsDomain,
+            },
+          ),
+        ),
+      );
+}
+
+String get _corsDomain {
+  final value = Platform.environment['CORS_DOMAIN'];
+  if (value == null) {
+    throw ArgumentError('CORS_DOMAIN is required to run the API');
+  }
+  return value;
 }
