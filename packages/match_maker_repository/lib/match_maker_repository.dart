36295f62d83781696ee668127d91b85e57780cc1
Will Hlas <75334479+willhlas@users.noreply.github.com>
2023-03-01 14:51:39 -0600

    Revert "feat(packages): initialize match maker repository"
    This reverts commit b4118706b6e48422ed7ec0f975260ee07b66389d.

--- packages/match_maker_repository/lib/match_maker_repository.dart
+++
@@ -1,4 +0,0 @@
-/// Repository for match making.
-library match_maker_repository;
-
-export 'src/match_maker_repository.dart';
--- packages/match_maker_repository/lib/src/match_maker_repository.dart
+++
@@ -1,7 +0,0 @@
-/// {@template match_maker_repository}
-/// Repository for match making.
-/// {@endtemplate}
-class MatchMakerRepository {
-  /// {@macro match_maker_repository}
-  const MatchMakerRepository();
-}
