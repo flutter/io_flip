c751a9b942baaf4379ea8b624684abe676c126ed
Will Hlas <75334479+willhlas@users.noreply.github.com>
2023-03-08 14:31:03 -0600

    feat: launch with emulators (#42)
    * feat: launch with emulators
    
    * apply code review

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:game_client/game_client.dart';
 import 'package:match_maker_repository/match_maker_repository.dart';
 import 'package:top_dash/app/app.dart';
@@ -5,16 +9,29 @@ import 'package:top_dash/bootstrap.dart';
 import 'package:top_dash/firebase_options_development.dart';
 import 'package:top_dash/settings/persistence/persistence.dart';
 
-void main() {
+void main() async {
   const gameClient = GameClient(
     endpoint: 'https://top-dash-dev-api-synvj3dcmq-uc.a.run.app',
   );
-  bootstrap(
-    firebaseOptions: DefaultFirebaseOptions.currentPlatform,
-    builder: (firestore) => App(
-      settingsPersistence: LocalStorageSettingsPersistence(),
-      gameClient: gameClient,
-      matchMakerRepository: MatchMakerRepository(db: firestore),
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  unawaited(
+    bootstrap(
+      (firestore, firebaseAuth) async {
+        final authenticationRepository = AuthenticationRepository(
+          firebaseAuth: firebaseAuth,
+        );
+        await authenticationRepository.signInAnonymously();
+
+        return App(
+          settingsPersistence: LocalStorageSettingsPersistence(),
+          gameClient: gameClient,
+          matchMakerRepository: MatchMakerRepository(db: firestore),
+        );
+      },
     ),
   );
 }
