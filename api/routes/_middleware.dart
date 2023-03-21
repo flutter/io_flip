b293863f40f4bc0e5ccf1a2d08ab0f9e3be92d03
Will Hlas <75334479+willhlas@users.noreply.github.com>
2023-03-21 15:49:36 -0500

    feat: add api encryption

--- api/routes/_middleware.dart
+++ api/routes/_middleware.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:cards_repository/cards_repository.dart';
 import 'package:dart_frog/dart_frog.dart';
+import 'package:encrypt/encrypt.dart';
 import 'package:game_script_machine/game_script_machine.dart';
 import 'package:logging/logging.dart';
 import 'package:match_repository/match_repository.dart';
@@ -19,6 +21,7 @@ Handler middleware(Handler handler) {
       .use(provider<ScriptsRepository>((_) => scriptsRepository))
       .use(provider<GameScriptMachine>((_) => gameScriptMachine))
       .use(jwtMiddleware.middleware)
+      .use(encryptionMiddleware)
       .use(
         fromShelfMiddleware(
           corsHeaders(
@@ -30,10 +33,41 @@ Handler middleware(Handler handler) {
       );
 }
 
+Middleware get encryptionMiddleware => (Handler handler) {
+      return (context) async {
+        final response = await handler(context);
+
+        final key = Key.fromUtf8(_encryptionKey);
+        final iv = IV.fromUtf8(_encryptionIV);
+        final encrypter = Encrypter(AES(key));
+
+        final body = jsonEncode(await response.body());
+        final encrypted = encrypter.encrypt(body, iv: iv).base64;
+
+        return response.copyWith(body: encrypted);
+      };
+    };
+
 String get _corsDomain {
   final value = Platform.environment['CORS_DOMAIN'];
   if (value == null) {
     throw ArgumentError('CORS_DOMAIN is required to run the API');
+  }
+  return value;
+}
+
+String get _encryptionKey {
+  final value = Platform.environment['ENCRYPTION_KEY'];
+  if (value == null) {
+    throw ArgumentError('ENCRYPTION_KEY is required to run the API');
+  }
+  return value;
+}
+
+String get _encryptionIV {
+  final value = Platform.environment['ENCRYPTION_IV'];
+  if (value == null) {
+    throw ArgumentError('ENCRYPTION_IV is required to run the API');
   }
   return value;
 }
