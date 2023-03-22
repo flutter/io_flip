4b7db0ba15df0c23fb9870ea404d3efe19c4c0cc
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-03-22 13:58:52 +0100

    feat: add initials form bloc and models

---
+++ lib/leaderboard/initials_form/bloc/initials_form_state.dart
@@ -0,0 +1,24 @@
+part of 'initials_form_bloc.dart';
+
+class InitialsFormState extends Equatable with FormzMixin {
+  InitialsFormState({
+    Initials? initials,
+  }) : initials = initials ?? Initials.pure();
+
+  final Initials initials;
+
+  InitialsFormState copyWith({
+    Initials? initials,
+    FormzSubmissionStatus? status,
+  }) {
+    return InitialsFormState(
+      initials: initials ?? this.initials,
+    );
+  }
+
+  @override
+  List<Object> get props => [initials];
+
+  @override
+  List<FormzInput> get inputs => [initials];
+}
---
+++ lib/leaderboard/initials_form/models/initials.dart
@@ -0,0 +1,51 @@
+import 'package:formz/formz.dart';
+import 'package:top_dash/l10n/l10n.dart';
+
+enum InitialsValidationError { invalid }
+
+extension InitialsValidationErrorX on InitialsValidationError? {
+  String? text(AppLocalizations l10n) {
+    switch (this) {
+      case InitialsValidationError.invalid:
+        return l10n.enterInitialsError;
+      case null:
+        return null;
+    }
+  }
+}
+
+class Initials extends FormzInput<String?, InitialsValidationError> {
+  Initials.pure([super.value = '']) : super.pure();
+  Initials.dirty([super.value = '']) : super.dirty();
+
+  final initialsRegex = RegExp('[A-Z]{3}');
+  static const initialsBlacklist = [
+    'FUK',
+    'FUC',
+    'COK',
+    'DIK',
+    'KKK',
+    'SHT',
+    'CNT',
+    'ASS',
+    'CUM',
+    'FAG',
+    'GAY',
+    'GOD',
+    'JEW',
+    'SEX',
+    'TIT',
+    'WTF',
+  ];
+
+  @override
+  InitialsValidationError? validator(String? value) {
+    if (value == null ||
+        value.isEmpty ||
+        !initialsRegex.hasMatch(value) ||
+        initialsBlacklist.contains(value)) {
+      return InitialsValidationError.invalid;
+    }
+    return null;
+  }
+}
