d76c8e1c4ad2de0bca2bf93b6038398be1ff5b8b
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-03-22 23:16:26 +0100

    test: leaderboard repository

--- api/packages/leaderboard_repository/test/src/leaderboard_repository_test.dart
+++ api/packages/leaderboard_repository/test/src/leaderboard_repository_test.dart
@@ -9,15 +9,46 @@ class _MockDbClient extends Mock implements DbClient {}
 void main() {
   group('LeaderboardRepository', () {
     late DbClient dbClient;
+    late LeaderboardRepository leaderboardRepository;
 
     setUp(() {
       dbClient = _MockDbClient();
+      leaderboardRepository = LeaderboardRepository(dbClient: dbClient);
     });
+
     test('can be instantiated', () {
       expect(
         LeaderboardRepository(dbClient: dbClient),
         isNotNull,
       );
+    });
+
+    group('getInitialsBlacklist', () {
+      const blacklistDocumentId = 'MdOoZMhusnJTcwfYE0nL';
+
+      const blacklist = ['AAA', 'BBB', 'CCC'];
+
+      test('returns the blacklist', () async {
+        when(() => dbClient.getById('initials_blacklist', blacklistDocumentId))
+            .thenAnswer(
+          (_) async => DbEntityRecord(
+            id: blacklistDocumentId,
+            data: const {
+              'blacklist': ['AAA', 'BBB', 'CCC'],
+            },
+          ),
+        );
+        final response = await leaderboardRepository.getInitialsBlacklist();
+        expect(response, equals(blacklist));
+      });
+
+      test('returns empty list if not found', () async {
+        when(() => dbClient.getById('initials_blacklist', any())).thenAnswer(
+          (_) async => null,
+        );
+        final response = await leaderboardRepository.getInitialsBlacklist();
+        expect(response, equals([]));
+      });
     });
   });
 }
