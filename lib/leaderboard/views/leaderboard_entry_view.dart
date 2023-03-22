9dd2d78baa5eccf3d4668de06515718d59e4609d
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-03-22 13:59:09 +0100

    feat: add initials form view

--- lib/leaderboard/views/leaderboard_entry_view.dart
+++ lib/leaderboard/views/leaderboard_entry_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:top_dash/l10n/l10n.dart';
+import 'package:top_dash/leaderboard/initials_form/initials_form.dart';
 import 'package:top_dash_ui/top_dash_ui.dart';
 
 class LeaderboardEntryView extends StatelessWidget {
@@ -36,114 +36,11 @@ class LeaderboardEntryView extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: TopDashSpacing.xxlg),
-              const _InitialsForm()
+              const InitialsForm()
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class _InitialsForm extends StatefulWidget {
-  const _InitialsForm();
-
-  @override
-  State<_InitialsForm> createState() => _InitialsFormState();
-}
-
-class _InitialsFormState extends State<_InitialsForm> {
-  final _formKey = GlobalKey<FormState>();
-  final _initialRegex = RegExp('[A-Z]{3}');
-  bool isValidForm = false;
-
-  static const initialsBlacklist = [
-    'FUK',
-    'FUC',
-    'COK',
-    'DIK',
-    'KKK',
-    'SHT',
-    'CNT',
-    'ASS',
-    'CUM',
-    'FAG',
-    'GAY',
-    'GOD',
-    'JEW',
-    'SEX',
-    'TIT',
-    'WTF',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    final l10n = context.l10n;
-    const white = TopDashColors.white;
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Container(
-            constraints: const BoxConstraints(minWidth: 100, maxWidth: 200),
-            child: TextFormField(
-              validator: (inputValue) {
-                if (inputValue == null ||
-                    inputValue.isEmpty ||
-                    !_initialRegex.hasMatch(inputValue) ||
-                    initialsBlacklist.contains(inputValue)) {
-                  return l10n.enterInitialsError;
-                }
-                return null;
-              },
-              textInputAction: TextInputAction.go,
-              inputFormatters: [
-                FilteringTextInputFormatter.allow(RegExp('[a-zA-Z]')),
-                UpperCaseTextFormatter(),
-                LengthLimitingTextInputFormatter(3)
-              ],
-              textCapitalization: TextCapitalization.characters,
-              cursorColor: white,
-              decoration: const InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: white),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: white),
-                ),
-                hintText: 'AAA',
-                hintStyle: TextStyle(color: white),
-              ),
-              textAlign: TextAlign.center,
-              style: const TextStyle(color: white),
-            ),
-          ),
-          const SizedBox(height: TopDashSpacing.xxlg),
-          OutlinedButton(
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {}
-            },
-            style: OutlinedButton.styleFrom(
-              side: const BorderSide(color: white),
-              foregroundColor: white,
-            ),
-            child: Text(l10n.continueButton.toUpperCase()),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class UpperCaseTextFormatter extends TextInputFormatter {
-  @override
-  TextEditingValue formatEditUpdate(
-    TextEditingValue oldValue,
-    TextEditingValue newValue,
-  ) {
-    return TextEditingValue(
-      text: newValue.text.toUpperCase(),
-      selection: newValue.selection,
     );
   }
 }
