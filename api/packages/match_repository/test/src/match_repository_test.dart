c1ff1885a6b9f90c7e3ff404870a1059530d3e0c
Hugo Walbecq <hugo@verygood.ventures>
2023-03-23 11:18:21 +0100

    test: update win tests

--- api/packages/match_repository/test/src/match_repository_test.dart
+++ api/packages/match_repository/test/src/match_repository_test.dart
@@ -554,6 +554,17 @@ void main() {
         test('updates correctly when host wins', () async {
           when(() => matchSolver.calculateMatchResult(any(), any()))
               .thenReturn(MatchResult.host);
+          when(() => dbClient.getById('score_cards', hostDeck.userId)).thenAnswer(
+                (_) async => DbEntityRecord(
+              id: hostDeck.userId,
+              data:const {
+                'wins': 0,
+                'currentStreak': 0,
+                'longestStreak': 0,
+                'currentDeck': 'deckId',
+              },
+            ),
+          );
 
           await matchRepository.playCard(
             matchId: matchId,
@@ -593,6 +604,17 @@ void main() {
         test('updates correctly when guest wins', () async {
           when(() => matchSolver.calculateMatchResult(any(), any()))
               .thenReturn(MatchResult.guest);
+          when(() => dbClient.getById('score_cards', guestDeck.userId)).thenAnswer(
+                (_) async => DbEntityRecord(
+              id: guestDeck.userId,
+              data:const {
+                'wins': 0,
+                'currentStreak': 0,
+                'longestStreak': 0,
+                'currentDeck': 'deckId',
+              },
+            ),
+          );
 
           await matchRepository.playCard(
             matchId: matchId,
