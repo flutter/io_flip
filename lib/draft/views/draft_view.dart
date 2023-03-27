d1423bfbbdec0f52c2d823331369d4d11f992220
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-03-27 18:56:38 +0200

    feat: deck selection screen redesign

--- lib/draft/views/draft_view.dart
+++ lib/draft/views/draft_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:top_dash/draft/draft.dart';
+import 'package:top_dash/gen/assets.gen.dart';
 import 'package:top_dash/l10n/l10n.dart';
 import 'package:top_dash/widgets/widgets.dart';
 import 'package:top_dash_ui/top_dash_ui.dart';
@@ -39,6 +40,45 @@ class DraftView extends StatelessWidget {
       );
     }
 
+    return Scaffold(
+      backgroundColor: TopDashColors.backgroundMain,
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const _DraftDeck(),
+                  Flexible(
+                    child: Container(
+                      constraints: const BoxConstraints(
+                        maxHeight: TopDashSpacing.xxxlg,
+                      ),
+                    ),
+                  ),
+                  const _SelectedDeck(),
+                ],
+              ),
+            ),
+            const _BottomBar(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// TODO(jaime): add arrows to go to next/prev card.
+// TODO(jaime): add swipe capability to go to next/prev card.
+class _DraftDeck extends StatelessWidget {
+  const _DraftDeck();
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.watch<DraftBloc>();
+    final state = bloc.state;
+
     final translateTween = Tween<Offset>(
       begin: Offset.zero,
       end: const Offset(0, 80),
@@ -49,106 +89,207 @@ class DraftView extends StatelessWidget {
       end: .8,
     );
 
-    return Scaffold(
-      backgroundColor: TopDashColors.backgroundMain,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                for (var i = state.cards.length - 1; i >= 0; i--)
-                  Transform.translate(
-                    offset: translateTween.transform(
-                      (i + 1) / state.cards.length,
-                    ),
-                    child: Transform.scale(
-                      scale: scaleTween.transform(
-                        (i + 1) / state.cards.length,
-                      ),
-                      child: GameCard(
-                        width: 120,
-                        height: 280,
-                        card: state.cards[i],
-                      ),
-                    ),
-                  ),
-              ],
+    const cardWidth = 230.0;
+    const cardHeight = 328.0;
+
+    final bottomPadding = translateTween.transform(1).dy -
+        ((cardHeight * (1 - scaleTween.transform(1))) / 2);
+
+    return Padding(
+      // Padding required to avoid widgets overlapping due to Stack child's
+      // translations
+      padding: EdgeInsets.only(bottom: bottomPadding),
+      child: Stack(
+        children: [
+          for (var i = state.cards.length - 1; i >= 0; i--)
+            Transform.translate(
+              offset: translateTween.transform(
+                (i + 1) / state.cards.length,
+              ),
+              child: Transform.scale(
+                scale: scaleTween.transform(
+                  (i + 1) / state.cards.length,
+                ),
+                child: GameCard(
+                  width: cardWidth,
+                  height: cardHeight,
+                  card: state.cards[i],
+                ),
+              ),
             ),
-            const SizedBox(height: 60),
-            if (state.status == DraftStateStatus.deckSelected) ...[
-              ElevatedButton(
+        ],
+      ),
+    );
+  }
+}
+
+class _SelectedDeck extends StatelessWidget {
+  const _SelectedDeck();
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.watch<DraftBloc>();
+    final selectedCards = bloc.state.selectedCards;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (var i = 0; i < 3; i++) ...[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: TopDashSpacing.xs),
+            child: MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: GestureDetector(
+                onTap: () {
+                  bloc.add(const SelectCard());
+                },
+                child: Container(
+                  height: 136,
+                  width: 104,
+                  decoration: BoxDecoration(
+                    color: TopDashColors.lightBlue99,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Stack(
+                    children: [
+                      if (i < selectedCards.length) ...[
+                        Center(
+                          child: Text(
+                            '${selectedCards[i].name}\n'
+                            '${selectedCards[i].power}',
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                      Positioned(
+                        bottom: TopDashSpacing.xs,
+                        right: TopDashSpacing.xs,
+                        child: Container(
+                          width: 32,
+                          height: 32,
+                          decoration: BoxDecoration(
+                            color: TopDashColors.lightBlue60,
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: Image.asset(Assets.images.add.path),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]
+      ],
+    );
+  }
+}
+
+class _BottomBar extends StatelessWidget {
+  const _BottomBar();
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.watch<DraftBloc>();
+    final state = bloc.state;
+    final l10n = context.l10n;
+
+    final viewPortHeight = MediaQuery.of(context).size.height;
+    final isSmall = viewPortHeight < 700;
+
+    return Container(
+      padding: const EdgeInsets.all(TopDashSpacing.sm),
+      height: isSmall ? 64 : 96,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: RoundedButton.icon(
+              const Icon(Icons.question_mark_rounded),
+              backgroundColor: Colors.white,
+              onPressed: () => GoRouter.of(context).go('/how_to_play'),
+            ),
+          ),
+          if (state.status == DraftStateStatus.deckSelected) ...[
+            Center(
+              child: RoundedButton.text(
+                l10n.joinMatch.toUpperCase(),
                 onPressed: () {
                   GoRouter.of(context).goNamed(
                     'match_making',
                     queryParams: {'cardId': state.selectedCardIds()},
                   );
                 },
-                child: Text(l10n.play),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  GoRouter.of(context).goNamed(
-                    'match_making',
-                    queryParams: {
-                      'createPrivateMatch': 'true',
-                      'cardId': state.selectedCardIds(),
-                    },
-                  );
-                },
-                child: const Text('Create private match'),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  final goRouter = GoRouter.of(context);
-                  final inviteCode = await showDialog<String?>(
-                    context: context,
-                    builder: (_) => const _JoinPrivateMatchDialog(),
-                  );
-                  if (inviteCode != null) {
-                    goRouter.goNamed(
-                      'match_making',
-                      queryParams: {
-                        'inviteCode': inviteCode,
-                        'cardId': state.selectedCardIds(),
-                      },
-                    );
-                  }
-                },
-                child: const Text('Join Private match'),
-              ),
-            ] else
-              Row(
+            ),
+          ] else ...[
+            Center(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      bloc.add(const NextCard());
-                    },
-                    child: Text(l10n.nextCard),
+                  Text(
+                    l10n.deckBuildingTitle,
+                    style: TopDashTextStyles.mobile.titleSmall,
                   ),
-                  ElevatedButton(
-                    onPressed: state.selectedCards.contains(state.cards.last)
-                        ? null
-                        : () {
-                            bloc.add(const SelectCard());
-                          },
-                    child: Text(l10n.useCard),
+                  const SizedBox(height: TopDashSpacing.xs),
+                  Text(
+                    l10n.deckBuildingSubtitle,
+                    style: TopDashTextStyles.mobile.bodySmall,
                   ),
                 ],
               ),
-            const SizedBox(height: 60),
-            Column(
-              children: [
-                for (final card in state.selectedCards)
-                  Text(
-                    '${card.name}: ${card.power}',
-                  ),
-              ],
-            ),
+            )
           ],
-        ),
+        ],
       ),
+    );
+  }
+}
+
+// TODO(jaime): check if private match feature is still required.
+class _BottomBar2 extends StatelessWidget {
+  const _BottomBar2();
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.watch<DraftBloc>();
+    final state = bloc.state;
+
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            GoRouter.of(context).goNamed(
+              'match_making',
+              queryParams: {
+                'createPrivateMatch': 'true',
+                'cardId': state.selectedCardIds(),
+              },
+            );
+          },
+          child: const Text('Create private match'),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            final goRouter = GoRouter.of(context);
+            final inviteCode = await showDialog<String?>(
+              context: context,
+              builder: (_) => const _JoinPrivateMatchDialog(),
+            );
+            if (inviteCode != null) {
+              goRouter.goNamed(
+                'match_making',
+                queryParams: {
+                  'inviteCode': inviteCode,
+                  'cardId': state.selectedCardIds(),
+                },
+              );
+            }
+          },
+          child: const Text('Join Private match'),
+        ),
+      ],
     );
   }
 }
