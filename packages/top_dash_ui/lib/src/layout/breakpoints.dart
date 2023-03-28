0d308c6fbe34865c816146cd16e944c5ff4a363d
Hugo Walbecq <hugo@verygood.ventures>
2023-03-28 17:03:36 +0200

    feat: add responsive layout

---
+++ packages/top_dash_ui/lib/src/layout/breakpoints.dart
@@ -0,0 +1,5 @@
+/// Namespace for Default TopDash Breakpoints
+abstract class TopDashBreakpoints {
+  /// Max width for a medium layout.
+  static const double medium = 1150;
+}
--- packages/top_dash_ui/lib/src/widgets/widgets.dart
+++ packages/top_dash_ui/lib/src/widgets/widgets.dart
@@ -1,4 +1,5 @@
 export 'card_overlay.dart';
 export 'flipped_game_card.dart';
 export 'game_card.dart';
+export 'responsive_layout.dart';
 export 'rounded_button.dart';
