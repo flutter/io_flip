cb89aff983ea8a693a368a4f4d24f090345682f2
Kirpal Demian <kirpal@verygood.ventures>
2023-03-29 11:34:04 -0400

    fix replace

--- packages/api_client/lib/src/api_client.dart
+++ packages/api_client/lib/src/api_client.dart
@@ -176,7 +176,7 @@ class ApiClient {
     Map<String, String>? queryParameters,
   }) async {
     final uri = _base.replace(
-      scheme: _base.replace('http', 'ws'),
+      scheme: _base.scheme.replaceAll('http', 'ws'),
       path: path,
       queryParameters: queryParameters,
     );
