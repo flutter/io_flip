d4f8e8ef20d480b511b829ab115248eeb0a9a84f
Hugo Walbecq <hugo@verygood.ventures>
2023-03-30 11:05:01 +0200

    feat: prompt white list (#121)
    * feat: prompt whitelist route and repository
    
    * chore: add missing workflows
    
    * chore: update endpoint route
    
    * chore: add whitelist id from env

---
+++ api/routes/game/prompt/whitelist.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:prompt_repository/prompt_repository.dart';
+
+FutureOr<Response> onRequest(RequestContext context) async {
+  if (context.request.method == HttpMethod.get) {
+    final leaderboardRepository = context.read<PromptRepository>();
+    final list = await leaderboardRepository.getPromptWhitelist();
+    return Response.json(body: {'list': list});
+  }
+  return Response(statusCode: HttpStatus.methodNotAllowed);
+}
