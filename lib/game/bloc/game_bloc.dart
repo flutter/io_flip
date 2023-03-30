833334b9d947b8cb583e9402081b7b9496bcd9af
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-03-30 09:43:40 +0200

    feat: stay when match over and opponent leaves

--- lib/game/bloc/game_bloc.dart
+++ lib/game/bloc/game_bloc.dart
@@ -200,8 +200,12 @@ class GameBloc extends Bloc<GameEvent, GameState> {
                     : match.hostConnected == false,
               );
       _opponentDisconnectSubscription =
-          opponentDisconnectStream.listen((state) {
-        emit(const OpponentAbsentState());
+          opponentDisconnectStream.listen((match) async {
+        final matchState = await _gameResource.getMatchState(match.id);
+        final matchOver = matchState?.isOver() ?? true;
+        if (!matchOver) {
+          emit(const OpponentAbsentState());
+        }
         completer.complete();
       });
 
