ac62772dcb9ec2968bd1aa4bd1b4143947fe04bb
Hugo Walbecq <hugo@verygood.ventures>
2023-03-30 14:13:23 +0200

    feat: prompt screen & flow

---
+++ lib/prompt/prompt_page.dart
@@ -0,0 +1,63 @@
+import 'package:flow_builder/flow_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:top_dash/l10n/l10n.dart';
+import 'package:top_dash/prompt/prompt_form_view.dart';
+
+class FlowData {
+  FlowData({this.character, this.power, this.environment});
+
+  final String? character;
+  final String? power;
+  final String? environment;
+
+  FlowData copyWithNewAttribute(String attribute) {
+    return FlowData(
+      character: character ?? attribute,
+      power: character != null ? power ?? attribute : null,
+      environment: power != null ? environment ?? attribute : null,
+    );
+  }
+}
+
+class PromptPage extends StatelessWidget {
+  const PromptPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlowBuilder<FlowData>(
+        state: FlowData(),
+        onGeneratePages: (data, pages) {
+          return [
+            MaterialPage(
+              child: PromptFormView(
+                title: context.l10n.characterPromptPageTitle,
+                subtitle: context.l10n.characterPromptPageSubtitle,
+                hint: context.l10n.characterPromptPageHint,
+                buttonIcon: Icons.arrow_forward,
+              ),
+            ),
+            if (data.character != null)
+              MaterialPage(
+                child: PromptFormView(
+                  title: context.l10n.powerPromptPageTitle,
+                  subtitle: context.l10n.powerPromptPageSubtitle,
+                  hint: context.l10n.powerPromptPageHint,
+                  buttonIcon: Icons.arrow_forward,
+                ),
+              ),
+            if (data.power != null)
+              MaterialPage(
+                child: PromptFormView(
+                  title: context.l10n.environmentPromptPageTitle,
+                  subtitle: context.l10n.environmentPromptPageSubtitle,
+                  hint: context.l10n.environmentPromptHint,
+                  buttonIcon: Icons.check,
+                ),
+              ),
+          ];
+        },
+      ),
+    );
+  }
+}
---
+++ test/prompt/prompt_page_test.dart
@@ -0,0 +1,59 @@
+import 'package:flow_builder/flow_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:top_dash/prompt/prompt.dart';
+
+import '../helpers/helpers.dart';
+
+void main() {
+  group('PromptPage', () {
+    testWidgets('renders character form correctly', (tester) async {
+      await tester.pumpSubject();
+
+      expect(find.byType(FlowBuilder<FlowData>), findsOneWidget);
+      expect(find.text(tester.l10n.characterPromptPageTitle), findsOneWidget);
+      expect(
+        find.text(tester.l10n.characterPromptPageSubtitle),
+        findsOneWidget,
+      );
+      expect(find.text(tester.l10n.characterPromptPageHint), findsOneWidget);
+      expect(find.byIcon(Icons.arrow_forward), findsOneWidget);
+    });
+
+    testWidgets('renders power form correctly', (tester) async {
+      await tester.pumpSubject();
+
+      await tester.tap(find.byIcon(Icons.arrow_forward));
+      await tester.pumpAndSettle();
+
+      expect(find.text(tester.l10n.powerPromptPageTitle), findsOneWidget);
+      expect(find.text(tester.l10n.powerPromptPageSubtitle), findsOneWidget);
+      expect(find.text(tester.l10n.powerPromptPageHint), findsOneWidget);
+      expect(find.byIcon(Icons.arrow_forward), findsOneWidget);
+    });
+
+    testWidgets('renders environment form correctly', (tester) async {
+      await tester.pumpSubject();
+
+      await tester.tap(find.byIcon(Icons.arrow_forward));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byIcon(Icons.arrow_forward));
+      await tester.pumpAndSettle();
+
+      expect(find.text(tester.l10n.environmentPromptPageTitle), findsOneWidget);
+      expect(
+        find.text(tester.l10n.environmentPromptPageSubtitle),
+        findsOneWidget,
+      );
+      expect(find.text(tester.l10n.environmentPromptHint), findsOneWidget);
+      expect(find.byIcon(Icons.check), findsOneWidget);
+    });
+  });
+}
+
+extension InitialsFormViewTest on WidgetTester {
+  Future<void> pumpSubject() async {
+    return pumpApp(const Scaffold(body: PromptPage()));
+  }
+}
