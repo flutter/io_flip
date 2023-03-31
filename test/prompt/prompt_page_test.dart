f164346d1d5db2f5685cea2101f8a3069fce71e9
Hugo Walbecq <hugo@verygood.ventures>
2023-03-31 11:24:00 +0200

    test: fix flow data test

--- test/prompt/prompt_page_test.dart
+++ test/prompt/prompt_page_test.dart
@@ -54,9 +54,9 @@ void main() {
     test('flow data correctly copies', () {
       const data1 = FlowData();
       final data2 = data1
-        .copyWithNewAttribute('character')
-        .copyWithNewAttribute('power')
-        .copyWithNewAttribute('environment');
+          .copyWithNewAttribute('character')
+          .copyWithNewAttribute('power')
+          .copyWithNewAttribute('environment');
 
       expect(
         data2,
