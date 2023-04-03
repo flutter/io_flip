72435bb2499d4a17f91950a00b5bc7adb9adc818
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-04-03 13:10:34 +0200

    feat: add card sizes

--- packages/top_dash_ui/gallery/lib/widgets/game_card_story.dart
+++ packages/top_dash_ui/gallery/lib/widgets/game_card_story.dart
@@ -7,18 +7,80 @@ class GameCardStory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const StoryScaffold(
+    const cardList = [
+      _SmallGameCardItem(size: TopDashCardSizes.xs, name: 'xs'),
+      _SmallGameCardItem(size: TopDashCardSizes.sm, name: 'sm'),
+      _GameCardItem(size: TopDashCardSizes.md, name: 'md'),
+      _GameCardItem(size: TopDashCardSizes.lg, name: 'lg'),
+      _GameCardItem(size: TopDashCardSizes.xl, name: 'xl'),
+      _GameCardItem(size: TopDashCardSizes.xxl, name: 'xxl'),
+    ];
+
+    return StoryScaffold(
       title: 'Game Card',
-      body: Center(
-        child: GameCard(
-          image:
-              'https://firebasestorage.googleapis.com/v0/b/top-dash-dev.appspot.com/o/public%2Fdash_3.png?alt=media',
-          name: 'Dash the Great',
-          suitName: 'earth',
-          power: 57,
-          height: 300,
-          width: 200,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: cardList,
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class _SmallGameCardItem extends StatelessWidget {
+  const _SmallGameCardItem({required this.size, required this.name});
+
+  final Size size;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(TopDashSpacing.lg),
+      child: Row(
+        children: [
+          Container(
+            width: size.width,
+            height: size.height,
+            decoration: BoxDecoration(
+              color: TopDashColors.seedBlue,
+              borderRadius: BorderRadius.circular(4),
+            ),
+          ),
+          const SizedBox(width: TopDashSpacing.md),
+          Text('$name (${size.width} x ${size.height})'),
+        ],
+      ),
+    );
+  }
+}
+
+class _GameCardItem extends StatelessWidget {
+  const _GameCardItem({required this.size, required this.name});
+
+  final Size size;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(TopDashSpacing.lg),
+      child: Row(
+        children: [
+          GameCard(
+            image:
+                'https://firebasestorage.googleapis.com/v0/b/top-dash-dev.appspot.com/o/public%2Fdash_3.png?alt=media',
+            name: 'Dash the Great',
+            suitName: 'earth',
+            power: 57,
+            width: size.width,
+            height: size.height,
+          ),
+          const SizedBox(width: TopDashSpacing.md),
+          Text('$name (${size.width} x ${size.height})'),
+        ],
       ),
     );
   }
