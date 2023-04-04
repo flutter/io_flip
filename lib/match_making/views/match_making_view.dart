c21b439d8ed1ae0201325c235b2029b3bdaff58e
Samantha O'Brien <Samantha@verygood.ventures>
2023-04-04 15:16:24 -0400

    Update match_making_view.dart

--- lib/match_making/views/match_making_view.dart
+++ lib/match_making/views/match_making_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_domain/game_domain.dart';
 import 'package:go_router/go_router.dart';
 import 'package:top_dash/game/views/game_page.dart';
+import 'package:top_dash/l10n/l10n.dart';
 import 'package:top_dash/match_making/match_making.dart';
 import 'package:top_dash_ui/top_dash_ui.dart';
 
@@ -35,23 +36,20 @@ class MatchMakingView extends StatelessWidget {
       builder: (context, state) {
         if (state.status == MatchMakingStatus.processing ||
             state.status == MatchMakingStatus.initial) {
-          return Scaffold(
-            backgroundColor: TopDashColors.seedWhite,
-            body: Center(
-              child: Column(
-                children: [
-                  const CircularProgressIndicator(),
-                  if (state.match?.inviteCode != null)
-                    ElevatedButton(
-                      onPressed: () {
-                        _setClipboardData(
-                          ClipboardData(text: state.match?.inviteCode),
-                        );
-                      },
-                      child: const Text('Copy invite code'),
-                    ),
-                ],
-              ),
+          return ResponsiveLayoutBuilder(
+            small: (_, __) => _WaitingForMatchView(
+              deck: deck,
+              setClipboardData: _setClipboardData,
+              inviteCode: state.match?.inviteCode,
+              title: TopDashTextStyles.headlineMobileH4Light,
+              subtitle: TopDashTextStyles.headlineMobileH6Light,
+            ),
+            large: (_, __) => _WaitingForMatchView(
+              deck: deck,
+              setClipboardData: _setClipboardData,
+              inviteCode: state.match?.inviteCode,
+              title: TopDashTextStyles.headlineH4Light,
+              subtitle: TopDashTextStyles.headlineH6Light,
             ),
           );
         }
@@ -89,6 +87,76 @@ class MatchMakingView extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class _WaitingForMatchView extends StatelessWidget {
+  const _WaitingForMatchView({
+    required this.title,
+    required this.subtitle,
+    required this.deck,
+    required this.setClipboardData,
+    this.inviteCode,
+  });
+  final List<Card> deck;
+  final String? inviteCode;
+  final Future<void> Function(ClipboardData) setClipboardData;
+  final TextStyle title;
+  final TextStyle subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: TopDashColors.seedWhite,
+      body: Column(
+        children: [
+          const Spacer(),
+          Text(
+            'context.l10n.findingMatch',
+            style: title,
+          ),
+          if (inviteCode == null)
+            Text(
+              'context.l10n.searchingForOpponents',
+              style: subtitle,
+            )
+          else
+            ElevatedButton(
+              onPressed: () {
+                setClipboardData(
+                  ClipboardData(text: inviteCode),
+                );
+              },
+              child: Text('context.l10n.copyInviteCode'),
+            ),
+          const SizedBox(height: TopDashSpacing.xxlg),
+          const FadingDotLoader(),
+          const Spacer(),
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: [
+          //     for (final card in deck)
+          //       Padding(
+          //         padding: const EdgeInsets.all(TopDashSpacing.xxs),
+          //         child: GameCard(
+          //           height: 150,
+          //           width: 100,
+          //           image: card.image,
+          //           name: card.name,
+          //           suitName: card.suit.name,
+          //           power: card.power,
+          //         ),
+          //       ),
+          //   ],
+          // ),
+          const SizedBox(height: TopDashSpacing.sm),
+          RoundedButton.text(
+            'context.l10n.matchmaking',
+            backgroundColor: TopDashColors.seedGrey90,
+          ),
+          const SizedBox(height: TopDashSpacing.xxlg),
+        ],
+      ),
     );
   }
 }
