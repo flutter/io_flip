174450ce5dfffb5c1b9c6daaea0257af0c3583dd
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-04-11 10:46:43 +0200

    feat: how to play page scaffold

--- lib/how_to_play/view/how_to_play_page.dart
+++ lib/how_to_play/view/how_to_play_page.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:styled_text/styled_text.dart';
+import 'package:top_dash/gen/assets.gen.dart';
 import 'package:top_dash/l10n/l10n.dart';
 import 'package:top_dash_ui/top_dash_ui.dart';
 
@@ -12,111 +15,300 @@ class HowToPlayPage extends StatelessWidget {
     );
   }
 
-  static const _gap = SizedBox(height: TopDashSpacing.lg);
-
   @override
   Widget build(BuildContext context) {
-    final l10n = context.l10n;
-    final textTheme = Theme.of(context).textTheme;
+    final pageController = PageController();
 
-    final steps = <int, String>{
-      1: l10n.howToPlayStepOneTitle,
-      2: l10n.howToPlayStepTwoTitle,
-      3: l10n.howToPlayStepThreeTitle,
-      4: l10n.howToPlayStepFourTitle,
-    };
+    const pages = <Widget>[
+      _Intro(),
+      _HandBuilding(),
+      _ElementsIntro(),
+      _ElementsFire(),
+      _ElementsAir(),
+      _ElementsMetal(),
+      _ElementsEarth(),
+      _ElementsWater(),
+    ];
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Align(
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 800),
-            child: Padding(
-              padding: const EdgeInsets.all(TopDashSpacing.lg * 2),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+      backgroundColor: TopDashColors.seedScrim,
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints.expand(width: 400, height: 584),
+          margin: const EdgeInsets.all(TopDashSpacing.sm),
+          padding: const EdgeInsets.all(TopDashSpacing.lg),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: TopDashColors.seedWhite,
+          ),
+          child: Stack(
+            children: [
+              Column(
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.only(
-                      top: TopDashSpacing.lg,
-                      bottom: TopDashSpacing.lg * 2,
-                    ),
-                    child: _Header(),
-                  ),
-                  _gap,
-                  Text(
-                    l10n.howToPlayTitle,
-                    style: textTheme.titleLarge?.copyWith(
-                      fontWeight: FontWeight.w700,
+                  Expanded(
+                    child: PageView(
+                      scrollBehavior: ScrollConfiguration.of(context).copyWith(
+                        dragDevices: {
+                          PointerDeviceKind.touch,
+                          PointerDeviceKind.mouse,
+                        },
+                      ),
+                      controller: pageController,
+                      children: pages,
                     ),
                   ),
-                  _gap,
-                  for (final step in steps.entries) ...[
-                    Text(
-                      '${step.key}. ${step.value}',
-                      style: textTheme.titleMedium,
-                    ),
-                    _gap,
-                  ],
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: TopDashSpacing.lg + TopDashSpacing.xxlg,
-                      bottom: TopDashSpacing.xxxlg,
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          l10n.howToPlayTip,
-                          style: textTheme.titleMedium,
-                        ),
-                        const SizedBox(height: TopDashSpacing.sm),
-                        Text(
-                          l10n.howToPlayTipEncouragement,
-                          style: textTheme.titleMedium?.copyWith(
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  OutlinedButton(
-                    onPressed: () => GoRouter.of(context).go('/prompt'),
-                    child: Text(l10n.howToPlayButtonText),
-                  ),
+                  RoundedButton.icon(
+                    const Icon(Icons.arrow_forward),
+                    onPressed: () {
+                      if (pageController.page == pages.length - 1) {
+                        maybePop(context);
+                      } else {
+                        pageController.nextPage(
+                          duration: const Duration(milliseconds: 250),
+                          curve: Curves.ease,
+                        );
+                      }
+                    },
+                  )
                 ],
               ),
-            ),
+              Align(
+                alignment: Alignment.topRight,
+                child: CloseButton(
+                  onPressed: () {
+                    maybePop(context);
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
+
+  void maybePop(BuildContext context) {
+    final router = GoRouter.of(context);
+    if (router.canPop()) {
+      router.pop();
+    }
+  }
 }
 
-class _Header extends StatelessWidget {
-  const _Header();
+class _Intro extends StatelessWidget {
+  const _Intro();
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    final style = Theme.of(context).textTheme.titleLarge;
-
-    return RichText(
-      textAlign: TextAlign.center,
-      text: TextSpan(
-        children: <InlineSpan>[
-          TextSpan(
-            text: '${l10n.ioBash} ',
-            style: style?.copyWith(
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          TextSpan(
-            text: l10n.howToPlayHeader,
-            style: style,
-          )
-        ],
-      ),
+    return Column(
+      children: [
+        const SizedBox(height: TopDashSpacing.xxlg),
+        Image.asset(
+          Assets.images.main.path,
+          width: 268,
+        ),
+        const SizedBox(height: TopDashSpacing.xxlg),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: TopDashSpacing.md),
+          child: _CustomText(l10n.howToPlayIntroTitle),
+        ),
+      ],
     );
+  }
+}
+
+class _HandBuilding extends StatelessWidget {
+  const _HandBuilding();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Column(
+      children: [
+        const SizedBox(height: TopDashSpacing.xxlg),
+        Image.asset(
+          Assets.images.main.path,
+          width: 268,
+        ),
+        const SizedBox(height: TopDashSpacing.xxlg),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: TopDashSpacing.md),
+          child: _CustomText(l10n.howToPlayDeckTitle),
+        ),
+      ],
+    );
+  }
+}
+
+class _ElementsIntro extends StatelessWidget {
+  const _ElementsIntro();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Column(
+      children: [
+        const SizedBox(height: TopDashSpacing.lg),
+        SizedBox(
+          height: 220,
+          width: 220,
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: ElementIcon.fire(),
+              ),
+              Align(
+                alignment: const Alignment(-1, -.1),
+                child: ElementIcon.water(),
+              ),
+              Align(
+                alignment: const Alignment(1, -.1),
+                child: ElementIcon.air(),
+              ),
+              Align(
+                alignment: const Alignment(-.6, 1),
+                child: ElementIcon.earth(),
+              ),
+              Align(
+                alignment: const Alignment(.6, 1),
+                child: ElementIcon.metal(),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: TopDashSpacing.xxlg),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: TopDashSpacing.md),
+          child: _CustomText(l10n.howToPlayElementsTitle),
+        ),
+      ],
+    );
+  }
+}
+
+class _ElementsFire extends StatelessWidget {
+  const _ElementsFire();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Column(
+      children: [
+        const SizedBox(height: TopDashSpacing.xxxlg),
+        _CustomText(l10n.howToPlayElementsFireTitle),
+      ],
+    );
+  }
+}
+
+class _ElementsAir extends StatelessWidget {
+  const _ElementsAir();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Column(
+      children: [
+        const SizedBox(height: TopDashSpacing.xxxlg),
+        _CustomText(l10n.howToPlayElementsAirTitle),
+      ],
+    );
+  }
+}
+
+class _ElementsMetal extends StatelessWidget {
+  const _ElementsMetal();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Column(
+      children: [
+        const SizedBox(height: TopDashSpacing.xxxlg),
+        _CustomText(l10n.howToPlayElementsMetalTitle),
+      ],
+    );
+  }
+}
+
+class _ElementsEarth extends StatelessWidget {
+  const _ElementsEarth();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Column(
+      children: [
+        const SizedBox(height: TopDashSpacing.xxxlg),
+        _CustomText(l10n.howToPlayElementsEarthTitle),
+      ],
+    );
+  }
+}
+
+class _ElementsWater extends StatelessWidget {
+  const _ElementsWater();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Column(
+      children: [
+        const SizedBox(height: TopDashSpacing.xxxlg),
+        _CustomText(l10n.howToPlayElementsWaterTitle),
+      ],
+    );
+  }
+}
+
+class _CustomText extends StatelessWidget {
+  const _CustomText(this.text);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return StyledText(
+      text: text,
+      style: textStyle(context),
+      tags: textTags(context),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  bool isScreenSmall(BuildContext context) =>
+      MediaQuery.of(context).size.width < TopDashBreakpoints.medium;
+
+  TextStyle textStyle(BuildContext context) {
+    return isScreenSmall(context)
+        ? TopDashTextStyles.headlineMobileH4Light
+        : TopDashTextStyles.headlineH4Light;
+  }
+
+  Map<String, StyledTextTagBase> textTags(BuildContext context) {
+    return {
+      'bold': StyledTextTag(
+        style: isScreenSmall(context)
+            ? TopDashTextStyles.headlineMobileH4
+            : TopDashTextStyles.headlineH4,
+      ),
+      'yellow': StyledTextTag(
+        style: const TextStyle(color: TopDashColors.seedYellow),
+      ),
+      'lightblue': StyledTextTag(
+        style: const TextStyle(color: TopDashColors.seedPaletteLightBlue80),
+      ),
+      'green': StyledTextTag(
+        style: const TextStyle(color: TopDashColors.seedGreen),
+      ),
+      'red': StyledTextTag(
+        style: const TextStyle(color: TopDashColors.seedRed),
+      ),
+      'blue': StyledTextTag(
+        style: const TextStyle(color: TopDashColors.seedBlue),
+      ),
+    };
   }
 }
