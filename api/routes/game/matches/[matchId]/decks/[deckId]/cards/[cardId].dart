305ad1b977cd59ee8faccc200261585f4a9876c7
Hugo Walbecq <hugo@verygood.ventures>
2023-04-14 14:05:13 +0200

    test: test pubsub pkg

--- api/routes/game/matches/[matchId]/decks/[deckId]/cards/[cardId].dart
+++ api/routes/game/matches/[matchId]/decks/[deckId]/cards/[cardId].dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
-import 'package:gcloud/pubsub.dart';
 import 'package:jwt_middleware/jwt_middleware.dart';
 import 'package:logging/logging.dart';
 import 'package:match_repository/match_repository.dart';
@@ -24,14 +23,6 @@ FutureOr<Response> onRequest(
         deckId: deckId,
         userId: user.id,
       );
-
-      final topic = await pubsubService.createTopic('playCard');
-
-      await topic.publish(Message.withString('message', attributes: {
-        'matchId': matchId,
-        'cardId': cardId,
-        'deckId': deckId,
-      }));
     } catch (e, s) {
       context.read<Logger>().severe('Error playing a move', e, s);
       rethrow;
