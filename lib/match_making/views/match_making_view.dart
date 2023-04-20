1b5923412c54bc917e9bf21739cab9544aa72a1e
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-04-20 15:01:04 +0200

    feat: match making ui

--- lib/match_making/views/match_making_view.dart
+++ lib/match_making/views/match_making_view.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart' hide Card;
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_domain/game_domain.dart';
+import 'package:go_router/go_router.dart';
 import 'package:top_dash/audio/audio.dart';
+import 'package:top_dash/game/game.dart';
 import 'package:top_dash/l10n/l10n.dart';
 import 'package:top_dash/match_making/match_making.dart';
 import 'package:top_dash_ui/top_dash_ui.dart';
@@ -26,44 +28,44 @@ class MatchMakingView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<MatchMakingBloc, MatchMakingState>(
       listener: (previous, current) {
-        // if (current.status == MatchMakingStatus.completed) {
-        //   Future.delayed(
-        //     const Duration(seconds: 3),
-        //     () => _routerNeglectCall(
-        //       context,
-        //       () => context.goNamed(
-        //         'game',
-        //         extra: GamePageData(
-        //           isHost: current.isHost,
-        //           matchId: current.match?.id ?? '',
-        //         ),
-        //       ),
-        //     ),
-        //   );
-        // }
+        if (current.status == MatchMakingStatus.completed) {
+          Future.delayed(
+            const Duration(seconds: 3),
+            () => _routerNeglectCall(
+              context,
+              () => context.goNamed(
+                'game',
+                extra: GamePageData(
+                  isHost: current.isHost,
+                  matchId: current.match?.id ?? '',
+                ),
+              ),
+            ),
+          );
+        }
       },
       builder: (context, state) {
-        // if (state.status == MatchMakingStatus.processing ||
-        //     state.status == MatchMakingStatus.initial) {
-        return ResponsiveLayoutBuilder(
-          small: (_, __) => _WaitingForMatchView(
-            deck: deck,
-            setClipboardData: _setClipboardData,
-            inviteCode: state.match?.inviteCode,
-            title: TopDashTextStyles.mobileH4Light,
-            subtitle: TopDashTextStyles.mobileH6Light,
-            key: const Key('small_waiting_for_match_view'),
-          ),
-          large: (_, __) => _WaitingForMatchView(
-            deck: deck,
-            setClipboardData: _setClipboardData,
-            inviteCode: state.match?.inviteCode,
-            title: TopDashTextStyles.headlineH4Light,
-            subtitle: TopDashTextStyles.headlineH6Light,
-            key: const Key('large_waiting_for_match_view'),
-          ),
-        );
-        // }
+        if (state.status == MatchMakingStatus.processing ||
+            state.status == MatchMakingStatus.initial) {
+          return ResponsiveLayoutBuilder(
+            small: (_, __) => _WaitingForMatchView(
+              deck: deck,
+              setClipboardData: _setClipboardData,
+              inviteCode: state.match?.inviteCode,
+              title: TopDashTextStyles.mobileH4Light,
+              subtitle: TopDashTextStyles.mobileH6Light,
+              key: const Key('small_waiting_for_match_view'),
+            ),
+            large: (_, __) => _WaitingForMatchView(
+              deck: deck,
+              setClipboardData: _setClipboardData,
+              inviteCode: state.match?.inviteCode,
+              title: TopDashTextStyles.headlineH4Light,
+              subtitle: TopDashTextStyles.headlineH6Light,
+              key: const Key('large_waiting_for_match_view'),
+            ),
+          );
+        }
 
         if (state.status == MatchMakingStatus.timeout) {
           return const Scaffold(
@@ -126,86 +128,85 @@ class _WaitingForMatchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final isSmall = screenWidth < TopDashBreakpoints.small;
     return Scaffold(
       body: Column(
         children: [
           Expanded(
-            child: ConstrainedBox(
-              constraints: const BoxConstraints(maxHeight: 600),
-              child: Column(
-                children: [
-                  const Spacer(),
-                  Text(
-                    context.l10n.findingMatch,
-                    style: title,
-                  ),
-                  if (inviteCode == null)
+            child: Center(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxHeight: 600),
+                child: Column(
+                  children: [
+                    const Spacer(),
                     Text(
-                      context.l10n.searchingForOpponents,
-                      style: subtitle,
-                    )
-                  else
-                    ElevatedButton(
-                      onPressed: () {
-                        final code = inviteCode;
-                        if (code != null) {
-                          setClipboardData(ClipboardData(text: code));
-                        }
-                      },
-                      child: Text(context.l10n.copyInviteCode),
+                      context.l10n.findingMatch,
+                      style: title,
                     ),
-                  const SizedBox(height: TopDashSpacing.xxlg),
-                  const FadingDotLoader(),
-                  const Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      for (final card in deck)
-                        Padding(
-                          padding: const EdgeInsets.all(TopDashSpacing.xs),
-                          child: Stack(
-                            children: [
-                              GameCard(
-                                height: 150,
-                                width: 100,
-                                image: card.image,
-                                name: card.name,
-                                suitName: card.suit.name,
-                                power: card.power,
-                              ),
-                              Positioned(
-                                bottom: TopDashSpacing.xs + 2,
-                                right: TopDashSpacing.xs,
-                                child: DecoratedBox(
-                                  decoration: BoxDecoration(
-                                    color: TopDashColors.seedWhite,
-                                    borderRadius: BorderRadius.circular(4),
-                                    border: Border.all(
-                                      width: 2,
-                                      color: TopDashColors.seedBlack,
-                                    ),
-                                    boxShadow: const [
-                                      BoxShadow(
-                                        offset: Offset(-2, 2),
-                                        spreadRadius: 1,
-                                      ),
-                                    ],
-                                  ),
-                                  child: const Icon(Icons.add, size: 32),
+                    if (inviteCode == null)
+                      Text(
+                        context.l10n.searchingForOpponents,
+                        style: subtitle,
+                      )
+                    else
+                      ElevatedButton(
+                        onPressed: () {
+                          final code = inviteCode;
+                          if (code != null) {
+                            setClipboardData(ClipboardData(text: code));
+                          }
+                        },
+                        child: Text(context.l10n.copyInviteCode),
+                      ),
+                    const SizedBox(height: TopDashSpacing.xxlg),
+                    const FadingDotLoader(),
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        for (final card in deck)
+                          Padding(
+                            padding: const EdgeInsets.all(TopDashSpacing.xs),
+                            child: Stack(
+                              children: [
+                                GameCard(
+                                  height: 150,
+                                  width: 100,
+                                  image: card.image,
+                                  name: card.name,
+                                  suitName: card.suit.name,
+                                  power: card.power,
                                 ),
-                              ),
-                            ],
+                                Positioned(
+                                  bottom: TopDashSpacing.xs + 2,
+                                  right: TopDashSpacing.xs,
+                                  child: DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: TopDashColors.seedWhite,
+                                      borderRadius: BorderRadius.circular(4),
+                                      border: Border.all(
+                                        width: 2,
+                                        color: TopDashColors.seedBlack,
+                                      ),
+                                      boxShadow: const [
+                                        BoxShadow(
+                                          offset: Offset(-2, 2),
+                                          spreadRadius: 1,
+                                        ),
+                                      ],
+                                    ),
+                                    child: const Icon(Icons.add, size: 32),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-          const SizedBox(height: TopDashSpacing.sm),
           IoFlipBottomBar(
             leading: const AudioToggleButton(),
             middle: RoundedButton.text(
@@ -214,7 +215,6 @@ class _WaitingForMatchView extends StatelessWidget {
               foregroundColor: TopDashColors.seedGrey70,
             ),
           ),
-          const SizedBox(height: TopDashSpacing.xxlg),
         ],
       ),
     );
