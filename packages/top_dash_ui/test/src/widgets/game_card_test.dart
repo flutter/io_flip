60be1dbab9c534e8c0394f93bbf037e507d37c9f
Erick Zanardo <erickzanardoo@gmail.com>
2023-04-20 15:58:18 -0300

    adding missing coverage

--- packages/top_dash_ui/test/src/widgets/game_card_test.dart
+++ packages/top_dash_ui/test/src/widgets/game_card_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
@@ -67,6 +69,95 @@ void main() {
 
         expect(tester.takeException(), isArgumentError);
       });
+    });
+
+    testWidgets('renders an overlay', (tester) async {
+      await mockNetworkImages(() async {
+        await tester.pumpWidget(
+          const Directionality(
+            textDirection: TextDirection.ltr,
+            child: GameCard(
+              image: 'image',
+              name: 'name',
+              description: 'description',
+              suitName: 'air',
+              power: 1,
+              overlay: CardOverlayType.win,
+            ),
+          ),
+        );
+
+        expect(find.byType(CardOverlay), findsOneWidget);
+      });
+    });
+  });
+
+  group('GameCardSize', () {
+    test('can be instantiated', () {
+      expect(GameCardSize.xs(), isNotNull);
+      expect(GameCardSize.sm(), isNotNull);
+      expect(GameCardSize.md(), isNotNull);
+      expect(GameCardSize.lg(), isNotNull);
+      expect(GameCardSize.xl(), isNotNull);
+      expect(GameCardSize.xxl(), isNotNull);
+    });
+
+    test('can scale by rect', () {
+      expect(
+        GameCardSize.xs().scaleByRect(
+          Rect.fromLTWH(0, 0, 200, 100),
+        ),
+        isA<GameCardSize>()
+            .having(
+              (size) => size.size.width,
+              'width',
+              equals(75),
+            )
+            .having(
+              (size) => size.size.height,
+              'height',
+              equals(100),
+            )
+            .having(
+              (size) => size.titleTextStyle.fontSize?.round(),
+              'title font size',
+              equals(12),
+            )
+            .having(
+              (size) => size.descriptionTextStyle.fontSize?.round(),
+              'description font size',
+              equals(6),
+            ),
+      );
+    });
+
+    test('can scale by rect with height bigger', () {
+      expect(
+        GameCardSize.xs().scaleByRect(
+          Rect.fromLTWH(0, 0, 100, 200),
+        ),
+        isA<GameCardSize>()
+            .having(
+              (size) => size.size.width,
+              'width',
+              equals(100),
+            )
+            .having(
+              (size) => size.size.height.round(),
+              'height',
+              equals(133),
+            )
+            .having(
+              (size) => size.titleTextStyle.fontSize?.round(),
+              'title font size',
+              equals(16),
+            )
+            .having(
+              (size) => size.descriptionTextStyle.fontSize?.round(),
+              'description font size',
+              equals(8),
+            ),
+      );
     });
   });
 }
