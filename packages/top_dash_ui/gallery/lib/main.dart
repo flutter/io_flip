0adb4fde7684c1740a7b6858699dcb89540d408a
Erick <erickzanardoo@gmail.com>
2023-04-20 17:41:33 -0300

    Update packages/top_dash_ui/gallery/lib/main.dart
    Co-authored-by: Kirpal Demian <kirpal@verygood.ventures>

--- packages/top_dash_ui/gallery/lib/main.dart
+++ packages/top_dash_ui/gallery/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:top_dash_ui/top_dash_ui.dart';
 
 void main() {
   final dashbook = Dashbook(
-    title: 'I/O Flip Dashbookk',
+    title: 'I/O Flip Dashbook',
     theme: TopDashTheme.themeData.copyWith(
       // Edits to make drawer and its text visible with the dark theme.
       cardColor: TopDashColors.seedBlack,
