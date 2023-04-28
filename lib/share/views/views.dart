0f9516befd60c34bcfcce62e4b3cfe177548b1e9
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-04-28 14:01:12 +0200

    refactor: remove unused share widgets

--- lib/share/views/views.dart
+++ lib/share/views/views.dart
@@ -1,6 +1,4 @@
 export 'card_inspector.dart';
 export 'share_card_dialog.dart';
-export 'share_dialog.dart';
 export 'share_hand_dialog.dart';
 export 'share_hand_page.dart';
-export 'share_page.dart';
