e6234470e1d68a9d8b16f11ce244092ed4bb1c78
Kirpal Demian <kirpal@verygood.ventures>
2023-05-04 16:13:46 -0400

    fix: firebase functions deploy

--- functions/src/deleteUserData.ts
+++ functions/src/deleteUserData.ts
@@ -1,7 +1,6 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-admin.initializeApp(functions.config().firebase);
 const firestore = admin.firestore();
 const storage = admin.storage();
 
--- functions/src/updateLeaderboard.ts
+++ functions/src/updateLeaderboard.ts
@@ -1,7 +1,6 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-admin.initializeApp(functions.config().firebase);
 const firestore = admin.firestore();
 
 export const updateLeaderboard = functions.firestore.document('score_cards/{card}').onUpdate(
