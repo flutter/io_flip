9839a5094a4fb547f6bfc221ecefaa9083ab1a8c
Erick Zanardo <erickzanardoo@gmail.com>
2023-05-05 11:43:39 -0300

    feat: flop docs

--- scripts/start_flop_webserver.sh
+++ scripts/start_flop_webserver.sh
@@ -5,6 +5,7 @@ echo ' ################################# '
 
 cd flop && flutter run \
   -d web-server \
+  --release \
   --dart-define ENCRYPTION_KEY=Bx677bkZEXvmiqzKXoph2mp3kDoqexkV \
   --dart-define ENCRYPTION_IV=rPMNKz2YzWQjjjMi \
   --dart-define RECAPTCHA_KEY=6LeafHolAAAAAH-kou5bR2y4gtEOmFXdd6pM4cJz \
