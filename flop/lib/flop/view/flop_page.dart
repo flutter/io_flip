f86fe27dd473b499481d0ba2ffca5d5a6f6fc4c1
Erick Zanardo <erickzanardoo@gmail.com>
2023-05-05 20:31:25 -0300

    feat: flop autoreload

--- flop/lib/flop/view/flop_page.dart
+++ flop/lib/flop/view/flop_page.dart
@@ -6,15 +6,18 @@ class FlopPage extends StatelessWidget {
   const FlopPage({
     super.key,
     required this.setAppCheckDebugToken,
+    required this.reload,
   });
 
   final void Function(String) setAppCheckDebugToken;
+  final void Function() reload;
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider<FlopBloc>(
       create: (_) => FlopBloc(
         setAppCheckDebugToken: setAppCheckDebugToken,
+        reload: reload,
       )..add(const NextStepRequested()),
       child: const FlopView(),
     );
--- scripts/spam_flop.sh
+++ scripts/spam_flop.sh
@@ -2,6 +2,6 @@
 PORT=$1
 for ((n=0;n<30;n++))
 do
-  open -na "Google Chrome" --args --new-window --incognito http://localhost:$PORT
+  open -na "Google Chrome" --args --new-window --incognito http://localhost:$PORT/index.html
   sleep 1
 done
