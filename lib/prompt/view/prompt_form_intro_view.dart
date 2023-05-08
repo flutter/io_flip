4c9117f581fbfe3989a6b85c2179f6c407c2daf6
Will Hlas <75334479+willhlas@users.noreply.github.com>
2023-05-08 12:04:58 -0500

    feat(prompt): add missing mute button

--- lib/prompt/view/prompt_form_intro_view.dart
+++ lib/prompt/view/prompt_form_intro_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:game_domain/game_domain.dart';
+import 'package:io_flip/audio/audio.dart';
 import 'package:io_flip/l10n/l10n.dart';
 import 'package:io_flip/prompt/prompt.dart';
 import 'package:io_flip_ui/io_flip_ui.dart';
@@ -13,60 +14,59 @@ class PromptFormIntroView extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = context.l10n;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: IoFlipSpacing.xlg),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Container(
-              alignment: Alignment.center,
-              constraints: const BoxConstraints(maxWidth: 516),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Stack(
-                      alignment: Alignment.topCenter,
-                      children: [
-                        const CardMaster(),
-                        Column(
-                          children: [
-                            SizedBox(
-                              height: CardMaster.cardMasterHeight -
-                                  (PromptFormIntroView._gap.height! * 2),
-                            ),
-                            Text(
-                              l10n.niceToMeetYou,
-                              style: IoFlipTextStyles.headlineH4Light,
-                              textAlign: TextAlign.center,
-                            ),
-                            const SizedBox(height: IoFlipSpacing.sm),
-                            Text(
-                              l10n.introTextPromptPage,
-                              style: IoFlipTextStyles.headlineH6Light,
-                              textAlign: TextAlign.center,
-                            ),
-                            PromptFormIntroView._gap,
-                          ],
-                        )
-                      ],
-                    ),
-                  ],
-                ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Container(
+            alignment: Alignment.center,
+            constraints: const BoxConstraints(maxWidth: 516),
+            padding: const EdgeInsets.symmetric(horizontal: IoFlipSpacing.xlg),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      const CardMaster(),
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: CardMaster.cardMasterHeight -
+                                (PromptFormIntroView._gap.height! * 2),
+                          ),
+                          Text(
+                            l10n.niceToMeetYou,
+                            style: IoFlipTextStyles.headlineH4Light,
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: IoFlipSpacing.sm),
+                          Text(
+                            l10n.introTextPromptPage,
+                            style: IoFlipTextStyles.headlineH6Light,
+                            textAlign: TextAlign.center,
+                          ),
+                          PromptFormIntroView._gap,
+                        ],
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
-          IoFlipBottomBar(
-            middle: RoundedButton.text(
-              l10n.letsGetStarted,
-              onPressed: () => context.updateFlow<Prompt>(
-                (data) => data.setIntroSeen(),
-              ),
+        ),
+        IoFlipBottomBar(
+          leading: const AudioToggleButton(),
+          middle: RoundedButton.text(
+            l10n.letsGetStarted,
+            onPressed: () => context.updateFlow<Prompt>(
+              (data) => data.setIntroSeen(),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
