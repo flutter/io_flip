80142a99278ab9c03f86bbfce341c861b92c0426
Samantha O'Brien <114935609+samanthaobrien2059@users.noreply.github.com>
2023-05-09 13:14:59 -0400

    fix: add tests to card share dialog (#454)

--- test/share/views/share_card_dialog_test.dart
+++ test/share/views/share_card_dialog_test.dart
@@ -2,7 +2,7 @@ import 'package:api_client/api_client.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:game_domain/game_domain.dart';
-import 'package:io_flip/share/views/share_card_dialog.dart';
+import 'package:io_flip/share/share.dart';
 import 'package:io_flip_ui/io_flip_ui.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
@@ -23,19 +23,45 @@ const card = Card(
 );
 
 void main() {
+  late ShareResource shareResource;
+  setUp(() {
+    shareResource = _MockShareResource();
+    when(() => shareResource.facebookShareCardUrl(any())).thenReturn('');
+    when(() => shareResource.twitterShareCardUrl(any())).thenReturn('');
+  });
   group('ShareCardDialog', () {
-    testWidgets('renders a GameCard', (tester) async {
-      await tester.pumpSubject();
+    testWidgets('renders', (tester) async {
+      await tester.pumpSubject(shareResource);
+      expect(find.byType(ShareDialog), findsOneWidget);
+    });
 
+    testWidgets('renders a CardShareDialog widget', (tester) async {
+      await tester.pumpSubject(shareResource);
+      expect(find.byType(ShareDialog), findsOneWidget);
+    });
+
+    testWidgets('renders a GameCard widget', (tester) async {
+      await tester.pumpSubject(shareResource);
       expect(find.byType(GameCard), findsOneWidget);
+    });
+
+    testWidgets('renders the description', (tester) async {
+      await tester.pumpSubject(shareResource);
+      expect(find.text(tester.l10n.shareCardDialogDescription), findsOneWidget);
+    });
+
+    testWidgets('gets the twitter and facebook share links', (tester) async {
+      await tester.pumpSubject(shareResource);
+
+      verify(() => shareResource.twitterShareCardUrl('')).called(1);
+      verify(() => shareResource.facebookShareCardUrl('')).called(1);
     });
   });
 }
 
 extension ShareCardDialogTest on WidgetTester {
-  Future<void> pumpSubject() async {
+  Future<void> pumpSubject(ShareResource shareResource) async {
     await mockNetworkImages(() {
-      final ShareResource shareResource = _MockShareResource();
       when(() => shareResource.twitterShareCardUrl(any())).thenReturn('');
       when(() => shareResource.facebookShareCardUrl(any())).thenReturn('');
       return pumpApp(
