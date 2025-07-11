0876ff86eeef0e8c080233ee9bc97cf26cb8a5ab
Brett Morgan <brettmorgan@google.com>
2025-07-12 07:26:45 +1000

    Drop `--web-renderer canvaskit` (#506)

--- scripts/build_production_game.sh
+++ scripts/build_production_game.sh
@@ -6,7 +6,6 @@ RECAPTCHA_KEY=$3
 
 flutter build web \
   -t lib/main_production.dart \
-  --web-renderer canvaskit \
   --dart-define SHARING_ENABLED=true \
   --dart-define ENCRYPTION_KEY=$ENCRYPTION_KEY \
   --dart-define ENCRYPTION_IV=$ENCRYPTION_IV \
